66b8d0711a870cc19e3528de8d659d885bc5f285
M-jay <markallan52@hotmail>
2020-06-09 18:39:28 +0100

    Update comment_helper.php
    Bootstrap

--- upload/helpers/comment_helper.php
+++ upload/helpers/comment_helper.php
@@ -45,19 +45,19 @@ function commenttable($res, $type = null)
             $delete = '[<a href="'.$site_config['SITEURL'].'/comments?id=' . $row["id"] . '&amp;type=' . $type . '&amp;delete=1">Delete</a>]&nbsp;';
         }
 
-        print('<div class="f-post f-border"><table cellspacing="0" width="100%">');
-        print('<tr class="p-title">');
+        print('<div class="container"><table class="table table-striped">');
+        print('<tr">');
         print('<th align="center" width="150"></th>');
         print('<th align="right">' . $edit . $delete . '[<a href="$site_config[SITEURL]/report?comment=' . $row["id"] . '">Report</a>] Posted: ' . date("d-m-Y \\a\\t H:i:s", utc_to_tz_time($row["added"])) . '<a id="comment' . $row["id"] . '"></a></th>');
         print('</tr>');
         print('<tr valign="top">');
         if ($CURUSER['edit_users'] == 'no' && $privacylevel == 'strong') {
-            print('<td class="f-border comment-details" align="left" width="150"><center><b>' . $postername . '</b><br /><i>' . $title . '</i><br /><img width="80" height="80" src="' . $avatar . '" alt="" /><br /><br />Uploaded: ---<br />Downloaded: ---<br />Ratio: ---<br /><br /><a href="$site_config[SITEURL]/users?id=' . $row["user"] . '"><img src="themes/' . $THEME . '/forums/icon_profile.png" border="" alt="" /></a> <a href="$site_config[SITEURL]/mailbox?compose&amp;id=' . $row["user"] . '"><img src="themes/' . $THEME . '/forums/icon_pm.png" border="0" alt="" /></a></center></td>');
+            print('<td align="left" width="150"><center><b>' . $postername . '</b><br /><i>' . $title . '</i><br /><img width="80" height="80" src="' . $avatar . '" alt="" /><br /><br />Uploaded: ---<br />Downloaded: ---<br />Ratio: ---<br /><br /><a href="$site_config[SITEURL]/users?id=' . $row["user"] . '"><img src="themes/' . $THEME . '/forums/icon_profile.png" border="" alt="" /></a> <a href="$site_config[SITEURL]/mailbox?compose&amp;id=' . $row["user"] . '"><img src="themes/' . $THEME . '/forums/icon_pm.png" border="0" alt="" /></a></center></td>');
         } else {
-            print('<td class="f-border comment-details" align="left" width="150"><center><b>' . $postername . '</b><br /><i>' . $title . '</i><br /><img width="80" height="80" src="' . $avatar . '" alt="" /><br /><br />Uploaded: ' . $useruploaded . '<br />Downloaded: ' . $userdownloaded . '<br />Ratio: ' . $userratio . '<br /><br /><a href="$site_config[SITEURL]/users?id=' . $row["user"] . '"><img src="themes/' . $THEME . '/forums/icon_profile.png" border="0" alt="" /></a> <a href="/mailbox?compose&amp;id=' . $row["user"] . '"><img src="themes/' . $THEME . '/forums/icon_pm.png" border="0" alt="" /></a></center></td>');
+            print('<td align="left" width="150"><center><b>' . $postername . '</b><br /><i>' . $title . '</i><br /><img width="80" height="80" src="' . $avatar . '" alt="" /><br /><br />Uploaded: ' . $useruploaded . '<br />Downloaded: ' . $userdownloaded . '<br />Ratio: ' . $userratio . '<br /><br /><a href="$site_config[SITEURL]/users?id=' . $row["user"] . '"><img src="themes/' . $THEME . '/forums/icon_profile.png" border="0" alt="" /></a> <a href="/mailbox?compose&amp;id=' . $row["user"] . '"><img src="themes/' . $THEME . '/forums/icon_pm.png" border="0" alt="" /></a></center></td>');
         }
 
-        print('<td class="f-border comment">' . $commenttext . '<hr />' . $usersignature . '</td>');
+        print('<td>' . $commenttext . '<hr />' . $usersignature . '</td>');
         print('</tr>');
         print('</table></div>');
         print('<br />');
