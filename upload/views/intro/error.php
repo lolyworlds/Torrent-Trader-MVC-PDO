86a5382ecf2fb634b9e6b599984f57128163e924
M-jay <markallan52@hotmail>
2020-08-05 08:33:04 +0100

    start intro
    set guide section for error reporting
    querys, models controllers etc

---
+++ upload/views/intro/error.php
@@ -0,0 +1,4 @@
+<?php
+begin_frame("Errors");
+	print ("<br /><center><font size='3' color='orange'>Intro into PDO</font><br /><font size='2' color='orange'>This is a rough guide to assist</font><br /> ~ ");
+          end_frame();
---
+++ upload/views/intro/header.php
@@ -0,0 +1,9 @@
+<center>
+<a href='<?php echo TTURL; ?>/intro'><button type="button" class="btn btn-sm btn-primary">Intro</button></a>&nbsp;
+<a href='<?php echo TTURL; ?>/intro/querys'><button type="button" class="btn btn-sm btn-primary">Querys</button></a>&nbsp;
+<a href='<?php echo TTURL; ?>/intro/models'><button type="button" class="btn btn-sm btn-primary">Model</button></a>&nbsp;
+<a href='<?php echo TTURL; ?>/intro/controllers'><button type="button" class="btn btn-sm btn-primary">Controllerss</button></a>&nbsp;
+<a href='<?php echo TTURL; ?>/intro/classes'><button type="button" class="btn btn-sm btn-primary">Classes</button></a>&nbsp;
+<a href='<?php echo TTURL; ?>/intro/views'><button type="button" class="btn btn-sm btn-primary">Views</button></a>&nbsp;
+<a href='<?php echo TTURL; ?>/intro/error'><button type="button" class="btn btn-sm btn-primary">Errors</button></a>&nbsp;<br><br>
+</center>
